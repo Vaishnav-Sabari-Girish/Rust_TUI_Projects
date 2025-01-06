2bf5dad5ac5e6c36da97c2461bf71969cbedfcd5
vaishnav_sabari_girish <forgamesonly684@gmail.com>
2025-01-06 14:24:52 +0530

    Part 2 images added

--- all_famous_ui_elements/UI_Elements.md
+++ all_famous_ui_elements/UI_Elements.md
@@ -26,3 +26,25 @@ In this file , we will create a TUI containing all the basic UI elements found i
 
 ![P1 Button output](images/Part1/button_p1_editview_out.png)
 
+<br><br>
+
+### Part - 2 Elements 
+
+![Empty](images/Part2/part_2_empty.png)
+
+<br><br>
+
+![Filled](images/Part2/part_2_filled.png)
+
+<br><br>
+
+![Point 1](images/Part2/point_1.png)
+
+<br><br>
+
+![Point 2](images/Part2/point_2.png)
+
+<br><br>
+
+![Point 3](images/Part2/point_3.png)
+
--- all_famous_ui_elements/src/main.rs
+++ all_famous_ui_elements/src/main.rs
@@ -1,3 +1,4 @@
+use cursive::align::*;
 use cursive::traits::*;
 use cursive::views::*;
 use cursive::Cursive;
@@ -10,6 +11,7 @@ fn main() {
         .child(TextView::new("This is TextView"))
         .child(Button::new("This is a Button", open_dialog_box))
         .child(Button::new("Click me for more - Part 1", part1))
+        .child(Button::new("Click me for more - Part 2", part2))
         .child(Button::new("Exit", |s| s.quit()));
 
     siv.add_layer(elements);
@@ -42,6 +44,37 @@ fn part1(s: &mut Cursive) {
     )
     .title("Part - 1 Widgets")
     .button("Back", back);
+    s.add_layer(dialog_elements);
+}
+
+fn part2(s: &mut Cursive) {
+    let dialog_elements = Dialog::around(
+        LinearLayout::vertical()
+            .child(TextView::new("TextArea"))
+            .child(TextArea::new().content(""))
+            .child(TextView::new("SelectView"))
+            .child(TextView::new("").with_name("text"))
+            .child(
+                SelectView::new()
+                    .h_align(HAlign::Center)
+                    .item("Point 1", 1)
+                    .item("Point 2", 2)
+                    .item("Point 3", 3)
+                    .on_submit(|s, item| {
+                        let chosen = match *item {
+                            1 => "Point 1",
+                            2 => "Point 2",
+                            3 => "Point 3",
+                            _ => unreachable!("No such Point"),
+                        };
+
+                        s.add_layer(Dialog::info(format!("You chose \"{}\"", chosen)));
+                    }),
+            ),
+    )
+    .title("Part - 2 Widgets")
+    .button("Back", back);
+
     s.add_layer(dialog_elements);
 }
 
